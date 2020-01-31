22647d97f11802292d69f83183ce10be62c0b610
jasonlee8318 <58414357+jasonlee8318@users.noreply.github.com>
2020-01-31 16:56:29 +0900

    Update iris_knn.R

--- iris_knn.R
+++ iris_knn.R
@@ -42,12 +42,30 @@ head(normal_iris)
 #species 열과 결합
 final_normal_iris <- data.frame(normal_iris, Species= iris$Species)
 
-#test data 셋 구성 (1-120행)
-normal_iris_train <- final_normal_iris[1:120,]
 
-#predict data 셋 구성 (121-150행)
-normal_iris_predict <- final_normal_iris[121:150,]
+#학습, 검정 데이터로 분리
+#seed 생성
+set.seed(123)
 
+#샘플 키값 저장
+idx <-sample(1:nrow(final_normal_iris),0.7*nrow(final_normal_iris))
+idx
+
+#  [1]  44 118  61 130 138   7  77 128  79  65 134  64  94 142  14 122  33   6 150 126 116  90  82 127  83  89  68  74  36
+# [30]  18 147 108 143 146   3  55  87  25 135  26  16  46  45  40  17  15 113  48  28 114   5 132 137  12  54  20  97  71
+# [59] 131  35  60   9  34  24  93  39  69 124  66 112 148  50  56   1  37 106  29 119 111   8 121  47 123  53 145  92 139
+# [88]  57 115  11  86  85  95  38  70  80  43 100 104  27  30  75 101  73  23
+
+#7:3비율로 분리
+normal_iris_train <- final_normal_iris[idx,]
+normal_iris_predict <- final_normal_iris[-idx,]
+
+#분리되 데이터 row 확인
+nrow(normal_iris_train)
+#[1] 105
+
+nrow(normal_iris_predict)
+#[1] 45
 
 #KNN 라이브러리호출
 library(class)
@@ -59,16 +77,31 @@ model <- knn(train=normal_iris_train[,-5], test=normal_iris_predict[,-5],cl=norm
 #모델확인
 summary(model)
 
+#    setosa versicolor  virginica 
+#        12         18         15
 
 #모델평가
 library(gmodels)
 CrossTable(x = normal_iris_predict$Species, y=model, prop.chisq = F)
 
 #                            | model 
-#normal_iris_predict$Species | versicolor |  virginica |  Row Total | 
-#----------------------------|------------|------------|------------|
-#                  virginica |          4 |         26 |         30 | 
-#                            |      0.133 |      0.867 |            | 
-#----------------------------|------------|------------|------------|
-#               Column Total |          4 |         26 |         30 | 
-#----------------------------|------------|------------|------------|
+#normal_iris_predict$Species |     setosa | versicolor |  virginica |  Row Total | 
+#----------------------------|------------|------------|------------|------------|
+#                     setosa |         12 |          0 |          0 |         12 | 
+#                            |      1.000 |      0.000 |      0.000 |      0.267 | 
+#                            |      1.000 |      0.000 |      0.000 |            | 
+#                            |      0.267 |      0.000 |      0.000 |            | 
+#----------------------------|------------|------------|------------|------------|
+#                 versicolor |          0 |         16 |          1 |         17 | 
+#                            |      0.000 |      0.941 |      0.059 |      0.378 | 
+#                            |      0.000 |      0.889 |      0.067 |            | 
+#                            |      0.000 |      0.356 |      0.022 |            | 
+#----------------------------|------------|------------|------------|------------|
+#                  virginica |          0 |          2 |         14 |         16 | 
+#                            |      0.000 |      0.125 |      0.875 |      0.356 | 
+#                            |      0.000 |      0.111 |      0.933 |            | 
+#                            |      0.000 |      0.044 |      0.311 |            | 
+#----------------------------|------------|------------|------------|------------|
+#               Column Total |         12 |         18 |         15 |         45 | 
+#                            |      0.267 |      0.400 |      0.333 |            | 
+#----------------------------|------------|------------|------------|------------|
